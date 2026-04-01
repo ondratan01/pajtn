c5fb87a36771439b1e56b88735d7d85b559c75a2
[Ondra Ilek] <[valid-email]>
2026-04-01 13:24:36 +0200

    light work no reaction

--- 9-1.4.2026.py
+++ 9-1.4.2026.py
@@ -54,17 +54,56 @@ def tri():
             max = vstup
             pocet+=1
 
-        if min<vstup:
-            min=vstup
-        if max>vstup:
-            max=vstup
+        if vstup != 0:
+            if min>vstup:
+                min=vstup
+            if max<vstup:
+                max=vstup
         
        
     inputseznam.pop(len(inputseznam)-1)
-    print(inputseznam)
-tri()
+    print("max je ",max,"min je ",min)
+    rozdil = max-min
+    minstring = ""
+    maxstring = ""
+
+    if(min <0):
+        minstring = (f"({min})")
+    else:
+        minstring = min
+
+    if(max <0):
+        maxstring = (f"({max})")
+    else:
+        maxstring = max
 
 
+    print(f"rouzdil {maxstring} - {minstring} je {rozdil}")
 
+def ctyri():
+    vstup = 1
+    nasobek = 0
+    inputseznam = []
+    endseznam = []
+    
+
+    while vstup!=0:
+        
+        vstup = int(input("zadejte cislo do listu: "))
+        inputseznam.append(vstup)
+
+    inputseznam.pop(len(inputseznam)-1)
+    nasobek = int(input("zadejte nasobek: "))
+
+    # for i in inputseznam:
+    #     if ((inputseznam[i] % nasobek) != 0):
+    #         endseznam.append(inputseznam[i])
+
+    for i in range(0, len(inputseznam)):
+        if ((inputseznam[i] % nasobek) != 0):
+            endseznam.append(inputseznam[i])
+
+    print(endseznam)
+ctyri()
 
 
